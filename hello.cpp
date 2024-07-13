1db073663bc5439f0b5a27c9100a3dc27e78cda6
Abhay Singh Pundir <106371072+abhay-pundir@users.noreply.github.com>
2024-07-13 11:25:32 +0530

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace  std;
+int main(){
+    //int n  = 1;
+    for(int i = 1; i <= 1000; i++){
+        if((7*i)%20 == 1){
+            cout<<i<<"   ";
+        }
+    }
+    
+    return 0;
+}
